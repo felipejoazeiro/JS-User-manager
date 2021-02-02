df492d50d49a11a8f188901eee4e8e64b5f87c72
Felipe Joazeiro <felipejoazeiro@gmail.com>
2021-02-02 13:51:39 -0200

    Erros e pequenos consertos

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -11,11 +11,16 @@ class UserController{
         
         this.formEl.addEventListener('submit',(event)=>{
             event.preventDefault()
+            let btn = this.formEl.querySelector("[type=submit]")
+            btn.disabled=true
             let values = this.getValues()
             this.getPhoto().then(
                 (content)=>{
                     values.photo = content
                     this.addLine(values)
+                    this.formEl.reset()
+                    btn.disabled = false
+                    
                 },
                 (e)=>{
                     console.error(e)
@@ -40,7 +45,11 @@ class UserController{
             fileReader.onerror = (e)=>{
                 reject(e)
             }
-            fileReader.readAsDataURL(file)
+            if(file){
+                fileReader.readAsDataURL(file)
+            } else{
+                resolve('dist/img/boxed-bg.jpg')
+            }
         })
 
     }
@@ -52,6 +61,8 @@ class UserController{
                 if(field.checked){
                     user[field.name] = field.value
                 }
+            }else if(field.name == "admin"){
+                user[field.name] = field.checked
             }else{
                 user[field.name] = field.value
             }
@@ -70,17 +81,19 @@ class UserController{
 
     addLine(dataUser){
     
-        this.tableEl.innerHTML = `
-        <tr>
-            <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
-            <td>${dataUser.name}</td>
-            <td>${dataUser.email}</td>
-            <td>${dataUser.admin}</td>
-            <td>${dataUser.data}</td>
-            <td>
-                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-            </td>
-        </tr>`
+        let tr = document.createElement('tr')
+        tr.innerHTML = `
+                <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
+                <td>${dataUser.name}</td>
+                <td>${dataUser.email}</td>
+                <td>${(dataUser.admin) ? "Sim" : "Não"}</td>
+                <td>${dataUser.data}</td>
+                <td>
+                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                </td>
+            `
+
+        this.tableEl.appendChild(tr)
     }
 }

cb31e27426b927ed8ba412924c28d44edb63ef88
Felipe Joazeiro <felipejoazeiro@gmail.com>
2021-02-01 23:20:15 -0200

    Recebendo dados

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-var fields = document.querySelectorAll("#form-user-create [name]")
-var user = {}
-
-
-document.getElementById('form-user-create').addEventListener('submit',(event)=>{
-    event.preventDefault()
-    fields.forEach((field,index)=>{
-        if(field.name == "gender"){
-            if(field.checked){
-                user[field.name] = field.value
-            }
-        }else{
-            user[field.name] = field.value
-        }
-    })
-    console.log(user)
-})
+let userController = new UserController('form-user-create','table-users')
---
+++ models/User.js
@@ -0,0 +1,12 @@
+class User{
+    constructor(name,gender,birth,country,email,password,photo,admin){
+        this.name  = name
+        this.gender = gender
+        this.birth = birth
+        this.email = email
+        this.country = country
+        this.email = password
+        this.photo = photo
+        this.admin = admin
+    }
+}
